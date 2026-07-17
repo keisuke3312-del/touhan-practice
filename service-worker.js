d4fcc5ef7759f4d6a5f57ac05b1a0dfd0ae27a93
hernagi <keisuke3312@icloud.com>
2026-07-18 03:38:56 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="touhan-practice-v13.2.0";
+const CACHE="touhan-practice-v13.2.1";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./data/questions.json","./version.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
